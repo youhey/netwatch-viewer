5cb9dda9b1ba53c4c8b91ed115071313c7001bde
IKEDA Youhei <youhey.ikeda@gmail.com>
2026-06-06 01:29:12 +0900

    setup xCode Project

---
+++ NetwatchViewer/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  NetwatchViewer
+//
+//  Created by 池田洋平 on 2026/06/06.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ NetwatchViewer/NetwatchViewerApp.swift
@@ -0,0 +1,17 @@
+//
+//  NetwatchViewerApp.swift
+//  NetwatchViewer
+//
+//  Created by 池田洋平 on 2026/06/06.
+//
+
+import SwiftUI
+
+@main
+struct NetwatchViewerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
